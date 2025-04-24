4b311593960942c6ad0ed989ae5dd8fe1be402de
GopiCHityala96 <chityalagopi111@gmail.com>
2025-04-24 11:59:00 +0530

    Break and Continue

---
+++ Break_and_Continue.py
@@ -0,0 +1,18 @@
+# n = 0
+# while n<5:
+#     print(n)
+#     n = n+1
+# n = 0
+# while n < 5:
+#     if n == 2:
+#         break
+#     print(n)
+#     n += 1
+
+n = 0
+while n<5:
+    if n == 2:
+        n = n+1
+        continue
+    print(n)
+    n = n+1
